1e3e1152c2704946a2f0781e903e64c7e026db6b
sadeepadilharap <sadeepadilharap@gmail.com>
2022-10-30 22:29:06 +0530

    Updated the code

--- cone-volume.py
+++ cone-volume.py
@@ -1,3 +1,7 @@
+print("-----------------------------------------")
+print("Let's find the volume of a cone")
+print("-----------------------------------------")
+
 h=float(input("Height of the cone = "))
 r=float(input("radius of the cone = "))
 
@@ -6,3 +10,5 @@ vol=(22/21)*r*r*h
 print("Height =",h )
 print("radius =",r)
 print("Volume of the cone =", vol)
+
+print("The End-----------------------------------")
